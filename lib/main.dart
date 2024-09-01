8268109cac287a0e15709fdb825d3770e8417a7a
Cahyono Wahyu Setiawan <cahyonowahyus123@gmail.com>
2024-09-01 18:26:09 +0700

    develop: add new state to showing selected item from fortunewheel

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 import 'package:just_audio/just_audio.dart';
-import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -20,25 +19,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       home: const SpinWheelScreen(),
     );
   }
@@ -54,12 +37,23 @@ class SpinWheelScreen extends StatefulWidget {
 class _SpinWheelScreenState extends State<SpinWheelScreen> {
   final player = AudioPlayer(); // For playing sound
   final StreamController<int> selected = StreamController<int>();
-  final items = ['Prize 1', 'Prize 2', 'Prize 3', 'Prize 4', 'Prize 5'];
+  final items = <String>[
+    'Grogu',
+    'Mace Windu',
+    'Obi-Wan Kenobi',
+    'Han Solo',
+    'Luke Skywalker',
+    'Darth Vader',
+    'Yoda',
+    'Ahsoka Tano',
+  ];
+
+  int selectedIndex = -1; // To store the selected index
 
   @override
   void initState() {
-    super.initState();
     _loadSound();
+    super.initState();
   }
 
   // Load the spin sound
@@ -80,24 +74,38 @@ class _SpinWheelScreenState extends State<SpinWheelScreen> {
       appBar: AppBar(
         title: const Text('Spin the Wheel Fortune'),
       ),
-      body: Center(
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            selected.add(
+              Fortune.randomInt(0, items.length),
+            );
+          });
+        },
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FortuneWheel(
-              selected: selected.stream,
-              items: [
-                for (var item in items) FortuneItem(child: Text(item)),
-              ],
-              onAnimationStart: _playSound, // Play sound when animation starts
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {
-                final randomIndex = Random().nextInt(items.length);
-                selected.add(randomIndex); // Spin to a random item
-              },
-              child: const Text("Spin"),
+            Expanded(
+              child: FortuneWheel(
+                animateFirst: false,
+                selected: selected.stream,
+                onAnimationStart: () {
+                  stopSound();
+                },
+                onAnimationEnd: () {
+                  _playSound();
+
+                  // Get the selected index from the stream and retrieve the item
+                  selected.stream.first.then((index) {
+                    setState(() {
+                      selectedIndex = index;
+                      _showSelectedItem(); // Display or use the selected item
+                    });
+                  });
+                },
+                items: [
+                  for (var it in items) FortuneItem(child: Text(it)),
+                ],
+              ),
             ),
           ],
         ),
@@ -107,94 +115,23 @@ class _SpinWheelScreenState extends State<SpinWheelScreen> {
 
   // Play the spin sound
   Future<void> _playSound() async {
+    await player.seek(Duration.zero); // Reset to the beginning of the sound
+
     await player.play();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Play the spin sound
+  Future<void> stopSound() async {
+    await player.stop();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  // Show the selected item
+  void _showSelectedItem() {
+    if (selectedIndex != -1) {
+      final selectedItem = items[selectedIndex];
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Selected: $selectedItem')),
+      );
+    }
   }
 }
